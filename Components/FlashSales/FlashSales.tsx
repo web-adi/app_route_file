3f6b6d6cea802448060217a3f8173534dcdfc4ac
web-adi <adityamondal282@gmail.com>
2025-01-15 08:35:11 +0530

    Revert "otp modal introduced"
    This reverts commit f4324d1fe362f9b2f6c701753493d7ab3c3328c9.

--- Components/FlashSales/FlashSales.tsx
+++ Components/FlashSales/FlashSales.tsx
@@ -1,38 +1,13 @@
-import {
-  FlashSalesWrap,
-  OtpModalWrap,
-} from "@/styles/StyledComponent/FlashSalesWrap";
-import React, { useState } from "react";
+import { FlashSalesWrap } from "@/styles/StyledComponent/FlashSalesWrap";
+import React from "react";
 import Container from "@mui/material/Container";
 import CommonHeader from "../CommonHeader/CommonHeader";
-import { Box, Button } from "@mui/material";
-import MuiModalWrapper from "@/ui/Modal/MuiModalWrapper/MuiModalWrapper";
-import OtpInput from "react-otp-input";
 
 const FlashSales = () => {
-  const [otp, setOtp] = useState("");
-  const [open, setOpen] = useState(false);
-  const handelToggel = () => {
-    setOpen(!open);
-  };
   return (
     <FlashSalesWrap className="commonGap">
       <Container fixed>
         <CommonHeader title="Today’s" sectionHead="Flash Sales" />
-        <Button onClick={handelToggel}>Open</Button>
-        <OtpModalWrap>
-          <MuiModalWrapper open={open} title="Otp" onClose={handelToggel}>
-            <Box className="otp_blk">
-              <OtpInput
-                value={otp}
-                onChange={setOtp}
-                numInputs={4}
-                renderSeparator={<span> </span>}
-                renderInput={(props) => <input {...props} />}
-              />
-            </Box>
-          </MuiModalWrapper>
-        </OtpModalWrap>
       </Container>
     </FlashSalesWrap>
   );
--- styles/StyledComponent/FlashSalesWrap.tsx
+++ styles/StyledComponent/FlashSalesWrap.tsx
@@ -1,12 +1,3 @@
-import { primaryColors } from "@/mui-theme/_muiPalette";
 import { Box, styled } from "@mui/material";
 
 export const FlashSalesWrap = styled(Box)``;
-
-export const OtpModalWrap = styled(Box)`
-
-
-  input {
-    width: 100px;
-  }
-`;
