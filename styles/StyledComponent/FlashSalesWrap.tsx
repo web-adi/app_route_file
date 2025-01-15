f4324d1fe362f9b2f6c701753493d7ab3c3328c9
web-adi <adityamondal282@gmail.com>
2025-01-15 08:26:17 +0530

    otp modal introduced

--- styles/StyledComponent/FlashSalesWrap.tsx
+++ styles/StyledComponent/FlashSalesWrap.tsx
@@ -1,3 +1,12 @@
+import { primaryColors } from "@/mui-theme/_muiPalette";
 import { Box, styled } from "@mui/material";
 
 export const FlashSalesWrap = styled(Box)``;
+
+export const OtpModalWrap = styled(Box)`
+
+
+  input {
+    width: 100px;
+  }
+`;
