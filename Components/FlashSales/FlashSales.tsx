320b195940c20e45b1ef9483f9b40b4745772939
web-adi <adityamondal282@gmail.com>
2025-01-16 00:39:05 +0530

    testing desing

--- Components/FlashSales/FlashSales.tsx
+++ Components/FlashSales/FlashSales.tsx
@@ -3,11 +3,107 @@ import React from "react";
 import Container from "@mui/material/Container";
 import CommonHeader from "../CommonHeader/CommonHeader";
 
+import { Box, Slider, Stack, Tab, Tabs, Typography } from "@mui/material";
+import EthIcon from "@/ui/icons/EthIcon";
+import Image from "next/image";
+import assets from "@/json/assets";
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
 const FlashSales = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
   return (
     <FlashSalesWrap className="commonGap">
       <Container fixed>
         <CommonHeader title="Today’s" sectionHead="Flash Sales" />
+
+        <Slider
+          defaultValue={30}
+          aria-label="Default"
+          valueLabelDisplay="auto"
+        />
+        <Box sx={{ width: "100%" }}>
+          <Box>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="basic tabs example"
+            >
+              <Tab
+                icon={<EthIcon />}
+                iconPosition="start"
+                label="ETH"
+                {...a11yProps(0)}
+              />
+
+              <Tab label="USDT" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+          <CustomTabPanel value={value} index={0}>
+            <Box className="tab_inr_content">
+              <Typography variant="body1" className="text">
+                ETH Balance: = $0.004875
+              </Typography>
+              <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Typography variant="body1">
+                  Help, My Wallet Won’t Connect!
+                </Typography>
+                <Typography variant="body1">
+                  Can’t find tokens in your wallet?
+                </Typography>
+              </Stack>
+            </Box>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            Item Two
+          </CustomTabPanel>
+        </Box>
+
+        <Box>
+          <Typography variant="body1" className="cart">
+            cert
+          </Typography>
+          <Image
+            src={assets.cart_img_one}
+            width={950}
+            height={270}
+            alt="cart_img"
+          />
+        </Box>
       </Container>
     </FlashSalesWrap>
   );
--- styles/StyledComponent/FlashSalesWrap.tsx
+++ styles/StyledComponent/FlashSalesWrap.tsx
@@ -1,3 +1,90 @@
 import { Box, styled } from "@mui/material";
 
-export const FlashSalesWrap = styled(Box)``;
+export const FlashSalesWrap = styled(Box)`
+  .MuiSlider-root {
+    height: 25px;
+    .MuiSlider-rail {
+      background: #ced1d9;
+      height: 40px;
+      padding: 7px;
+      border-radius: 50px;
+    }
+    .MuiSlider-track {
+      background: linear-gradient(116.39deg, #4b28a9 0%, #c655ae 63.63%);
+      border: none;
+      left: 7px !important;
+    }
+
+    .MuiSlider-thumb {
+      display: none;
+    }
+  }
+
+  .MuiTabs-root {
+    .MuiTabs-scroller {
+      .MuiTabs-flexContainer {
+        .utr_blk {
+          background: rgba(206, 209, 217, 0.2);
+          padding: 9px 13px;
+          border-radius: 50px;
+        }
+        button {
+          min-height: auto;
+          padding: 12px 120px;
+          background: rgba(243, 243, 243, 1);
+          color: #020202;
+          position: relative;
+          &::before {
+            content: "";
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background: rgba(206, 209, 217, 0.2);
+            padding: 9px 13px;
+          }
+          &:not(:last-child) {
+            margin-right: 16px;
+          }
+          &.Mui-selected {
+            background: linear-gradient(
+              114.23deg,
+              rgba(132, 214, 252, 0.6) 21.89%,
+              rgba(246, 170, 209, 0.4) 77.98%
+            );
+          }
+
+          border-radius: 50px;
+        }
+      }
+
+      .MuiTabs-indicator {
+        display: none;
+      }
+    }
+  }
+
+  .tab_inr_content {
+    .text {
+      text-align: center;
+      position: relative;
+      &::before {
+        content: "";
+        position: absolute;
+        width: 40%;
+        height: 1px;
+        background: rgba(206, 209, 217, 1);
+        left: 0;
+        top: 50%;
+      }
+      &::after {
+        content: "";
+        position: absolute;
+        width: 40%;
+        height: 1px;
+        background: rgba(206, 209, 217, 1);
+        right: 0;
+        top: 50%;
+      }
+    }
+  }
+`;
